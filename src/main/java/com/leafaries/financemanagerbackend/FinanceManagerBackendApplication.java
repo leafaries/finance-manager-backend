60a1eef8e3de9ba1ec41c7b79e8dee280019cafd
Maciej Konopka <leafaries@proton.me>
2024-10-16 13:00:56 +0200

    Initial commit

---
+++ src/main/java/com/leafaries/financemanagerbackend/FinanceManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leafaries.financemanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceManagerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leafaries/financemanagerbackend/FinanceManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leafaries.financemanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
